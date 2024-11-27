610a5d0457c610296a0120eb45105902796f24b0
iamajay01 <ajaykarthik.eng@gmail.com>
2024-11-27 14:01:01 +0530

    renaming env

--- src/configs/dbConfig.ts
+++ src/configs/dbConfig.ts
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 export const connectDB = async () => {
   try {
-    await mongoose.connect(process.env.mongo_url!);
+    await mongoose.connect(process.env.MONGO_URL!);  
     console.log("MongoDB connected");
   } catch (error) {
     console.log(error);

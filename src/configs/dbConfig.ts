1d0d3963052e166b43758f46f159bd7a9eb7bf33
iamajay01 <ajaykarthik.eng@gmail.com>
2024-11-27 14:13:12 +0530

    renaming env

--- src/configs/dbConfig.ts
+++ src/configs/dbConfig.ts
@@ -2,9 +2,14 @@ import mongoose from "mongoose";
 
 export const connectDB = async () => {
   try {
-    await mongoose.connect(process.env.MONGO_URL!);  
+    const dbUri = process.env.MONGO_URL;
+    console.log("MongoDB URI:", dbUri); // Log the URI to see its value
+    if (!dbUri) {
+      throw new Error("MongoDB URI is not defined.");
+    }
+    await mongoose.connect(dbUri);
     console.log("MongoDB connected");
   } catch (error) {
-    console.log(error);
+    console.log("Error connecting to MongoDB:", error);
   }
 };
